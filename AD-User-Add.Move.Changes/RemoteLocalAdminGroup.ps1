d64265bc6adcae284d394d9d04c55e8d34130099
micahboggs <mboggs@NewMicah.smusd.local>
2015-10-19 09:39:33 -0700

    New Script - List members of local admin groups at a site

---
+++ AD-User-Add.Move.Changes/RemoteLocalAdminGroup.ps1
@@ -0,0 +1,14 @@
+﻿$computers = Get-ADComputer -SearchBase "ou=do,ou=smusd,dc=smusd,dc=local" -filter *
+
+
+
+foreach ($computer in $computers.name) {
+    write-host $computer
+    if(Test-Connection -ComputerName $computer -count 1 -Quiet) {
+        Invoke-Command { 
+            net localgroup administrators | where {$_ -AND $_ -notmatch "command completed successfully"} | select -skip 4 
+        } -computer $computer 
+    } else {
+        write-host "$computer offline"
+    }
+}
