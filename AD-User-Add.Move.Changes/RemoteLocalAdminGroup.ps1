809bf76e1ae945a8d5b8394ef0394c6ef21eb885
micahboggs <mboggs@NewMicah.smusd.local>
2015-10-21 15:08:57 -0700

    RemoteLocalAdminGroup.ps1
    Made it work. Added a progress bar as it takes a while to run. Other
    stuff.

--- AD-User-Add.Move.Changes/RemoteLocalAdminGroup.ps1
+++ AD-User-Add.Move.Changes/RemoteLocalAdminGroup.ps1
@@ -1,14 +1,65 @@
-﻿$computers = Get-ADComputer -SearchBase "ou=do,ou=smusd,dc=smusd,dc=local" -filter *
+﻿
+
+function GoGoGo {
+
+    $computers = Get-ADComputer -SearchBase "ou=do,ou=smusd,dc=smusd,dc=local" -filter * -Properties LastLogonDate
+    #$computers = Get-ADComputer "DO-PUR-813440" -Properties LastLogonDate
+    $i=1
+    $computercount=$computers.count
+
+    foreach ($computer in $computers) {
+            $computername = $computer.name
+            $percent = [math]::round($i / $computercount*100)
+
+            Write-Progress -Activity "($percent%) Finding out who's an Admin" -Status  "Looking up $computername" -PercentComplete  $percent
+            $i++
+            #Reset the failures or set if first one
+            $failures = @()
+            $administrators = @()
 
 
 
-foreach ($computer in $computers.name) {
-    write-host $computer
-    if(Test-Connection -ComputerName $computer -count 1 -Quiet) {
-        Invoke-Command { 
-            net localgroup administrators | where {$_ -AND $_ -notmatch "command completed successfully"} | select -skip 4 
-        } -computer $computer 
-    } else {
-        write-host "$computer offline"
+            $Out = '' | Select-Object Computer, LastLogon, Administrators, failures
+            $Out.Computer = $computer.name
+            $Out.LastLogon = $computer.LastLogonDate
+
+
+    
+        if(Test-Connection -ComputerName $computer.name -count 1 -Quiet) {
+            try
+            {
+                #if ($true) {
+                if (test-wsman $computer.name -ErrorAction Stop) {
+            
+                    try{
+                        $administrators = Invoke-Command { 
+                            net localgroup administrators | where {$_ -AND $_ -notmatch "command completed successfully"} | select -skip 4 
+                        } -computer $computer.name 
+                    } 
+                    catch {
+                    
+                        $failures += "Cannot lookup info"
+                    }
+                }
+            
+            }
+            catch 
+            {
+            
+                $failures += "WinRM not enabled"
+                continue
+            }
+        } else {
+        
+            $failures += "offline"
+        }
+
+        $out.Administrators = $administrators -join ';'
+        $out.failures = $failures -join ';'
+        $out 
+
+        Remove-Variable out, failures, administrators
     }
 }
+
+GoGoGo | export-csv "DOAdmins.csv" -NoTypeInformation
