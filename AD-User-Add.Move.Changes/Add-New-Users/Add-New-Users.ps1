772af2435fee117ff4ad30c14f495236205f5485
micahboggs <mboggs@smusd.org>
2016-09-22 14:31:15 -0700

    Added slash to name pattern
    Since HR likes to put slashes in job titles.

--- AD-User-Add.Move.Changes/Add-New-Users/Add-New-Users.ps1
+++ AD-User-Add.Move.Changes/Add-New-Users/Add-New-Users.ps1
@@ -15,7 +15,7 @@ Import-module ActiveDirectory
 
 ####### Region Configuration #########
  
-    $Version="1.2.2"
+    $Version="1.2.3"
 
 
     # Uncomment this if testing and you don't want it to send out emails
@@ -384,7 +384,7 @@ param(
 
         #Sanitize the strings
         $pattern ='[^a-zA-Z.]'
-        $namePattern = "[^a-zA-Z0-9.' '`'-]"
+        $namePattern = "[^a-zA-Z0-9.' '`'-/]"
 
         $GivenName = $User.GivenName -replace $namepattern,''
         $Surname = $User.Surname -replace $namepattern,''
