840f43e1dcd6648587469be9eae30127b9b9ad10
micahboggs <mboggs@smusd.org>
2018-03-22 13:55:01 -0700

    Created VMwareDeleteOldDMPFiles.ps1
    Script to auto delete old dmp files until problem can be fixed.

---
+++ VMwareDeleteOldDMPFiles.ps1
@@ -0,0 +1,16 @@
+﻿#script to remove old dmp files from VMware Update Manager Logs
+
+# How many days of dump files should be retained
+$daystokeep = 30
+
+
+######### Do not edit below this line ###########
+$limit = (get-date).adddays(-$daystokeep)
+$path = "C:\ProgramData\VMware\VMware Update Manager\Logs\*.dmp"
+
+
+
+
+# Execute
+get-childitem -path $path | Where-Object { $_.LastWriteTime -lt $limit } | Remove-Item -whatif
+
